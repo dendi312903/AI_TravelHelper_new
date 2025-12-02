20ac8141c89415f220a681a8f446a952f65a360e
Stepan Komarov <@dendi39>
2025-12-02 21:19:55 +0300

    refactor recomendation system (add recomendation by vector)

--- recomendating_system.py
+++ recomendating_system.py
@@ -1,30 +1,28 @@
-# Список пользователей
-users = ["user_1", "user_2", "user_3"]
+from math import sqrt
+# Кино, искусство, спорт
+user = [2, 1, 1]   
+cinema = [2, 1, 0] 
+stadium = [0, 0, 1]     
 
-# Список городских мест
-places = ["museum", "park", "cinema", "cafe", "library"]
+def cosine_simularity(vec1, vec2):
+    # Скалярное произведение
+    product = sum(a * b for a, b in zip(vec1, vec2))
+    # Длина первого вектора vec1
+    lenghvec1 = 0
+    for i in range(len(vec1)):
+        lenghvec1 += vec1[i] * vec1[i]
+    lengh1 = sqrt(lenghvec1)
+    
+    # Длина второго вектора vec2
+    lenghvec2 = 0
+    for i in range(len(vec2)):
+        lenghvec2 += vec2[i] * vec2[i]
+    lengh2 = sqrt(lenghvec2)
+    
+    if lengh1 == 0 or lengh2 == 0:
+        return 0
+    # Формула косинусного сходства (A * B) / (|A*A| * |B*B|)
+    return product / (lengh1 * lengh2)
 
-# Данные о посещениях / лайках пользователей
-interactions = {
-    "user_1": ["museum", "park"],
-    "user_2": ["cinema", "cafe"],
-    "user_3": ["library"]
-}
-
-
-def recommend(user_id, top_n=3):
-    # Пользовательские места
-    user_places = set(interactions[user_id])
-
-    # Все остальные места
-    all_places = set(places)
-    candidates = all_places - user_places
-
-    print(f"\nРекомендации для {user_id}:")
-    for place in list(candidates)[:top_n]:
-        print(" •", place)
-
-recommend("user_1")
-recommend("user_2")
-recommend("user_3")
-
+print("Сходства пользователя и стадиона:", cosine_simularity(user, stadium))
+print("Сходства пользователя и кино:", cosine_simularity(user, cinema))
